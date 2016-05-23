a00768bf7ba3d82142728bf7a1f2f88fce6d3f13
Yulong Gan <ganyulongnyu@gmail.com>
2016-05-22 17:45:33 -0700

    Add DELETE todos route

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
-//GET /todos/:id
+//GET /todos/:id   app.get(url, callback function)
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -43,6 +43,19 @@ app.post('/todos', function (req, res) {
 	todos.push(body);
 
 	res.json(body);
+});
+
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function () {
